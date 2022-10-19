7e2219c329bb6503ec380e361336c210b757e685
Pavel <kulikov-pavlik@bk.ru>
2022-10-19 19:03:19 +0300

    Removed useless arg in ParsingError Exception class

--- src/main/java/Exceptions/ParsingError.java
+++ src/main/java/Exceptions/ParsingError.java
@@ -5,13 +5,9 @@ import Colors.ConsoleColors;
 public class ParsingError extends Exception{
     private final String notExistingFileName;
     private final String errorMessage;
-    public ParsingError(String errorMessage, String fileName, int problemPos){
+    public ParsingError(String errorMessage, String fileName){
         this.notExistingFileName = fileName;
         this.errorMessage = errorMessage;
-    }
-
-    public ParsingError(String errorMessage, String fileName){
-        this(errorMessage, fileName, 0);
     }
 
     @Override
